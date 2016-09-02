d1a8a4a13851b1d87317081fca3a34185817f466
Mirrgie Riana <tacticsrealize2@gmail.com>
2016-09-02 20:38:53 +0900

    util: add: src

---
+++ mirrg.helium.standard.hydrogen.util/src/main/java/mirrg/helium/standard/hydrogen/util/HLambda.java
@@ -0,0 +1,36 @@
+package mirrg.helium.standard.hydrogen.util;
+
+import java.util.function.Consumer;
+import java.util.function.ObjIntConsumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class HLambda
+{
+
+	public static <T> T process(T object, Consumer<T> consumer)
+	{
+		consumer.accept(object);
+		return object;
+	}
+
+	public static <T> T get(Supplier<T> supplier)
+	{
+		return supplier.get();
+	}
+
+	/**
+	 * 添え字番号付きfor
+	 */
+	public static <T> void forEach(Stream<T> stream, ObjIntConsumer<T> consumer)
+	{
+		Integer[] i = new Integer[] {
+			0,
+		};
+		stream.sequential().forEach(object -> {
+			consumer.accept(object, i[0]);
+			i[0]++;
+		});
+	}
+
+}
